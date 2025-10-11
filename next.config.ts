d9d6ad87731a5786614cfe79488834c9ad8ce6fa
wa1ead <waliddraa2@gmail.com>
2025-10-11 18:15:52 +0100

    ppr

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    ppr: "incremental",
-  },
+    cacheComponents: true,
+  } as any,
 };
 
 export default nextConfig;
