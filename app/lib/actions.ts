491a25ff84c01e78c426bdd97ce02fd3c7a19531
wa1ead <waliddraa2@gmail.com>
2025-10-16 09:51:58 +0100

    adding authentication

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,11 @@
 "use server";
+
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 import postgres from "postgres";
-
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 export type State = {
   errors?: {
@@ -14,6 +15,27 @@ export type State = {
   };
   message?: string | null;
 };
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
